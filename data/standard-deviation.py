d99b9a28a255ee7e72c926d732b993d1718ddeba
dmslob <dmytro.slobodenyuk@gmail.com>
2026-03-14 21:57:47 +0200

    standard-deviation is updated

--- data/standard-deviation.py
+++ data/standard-deviation.py
@@ -1,7 +1,18 @@
 import numpy
 import math
 
-
+# Standard Deviation = how spread out the values are = Square root of Variance
+# 1. Find mean
+# 2. Find difference from mean for each values
+# 3. Find the square for each difference
+# 4. Find the average of squared differences (variance)
+# 5. Find the square root of variance (standard deviation)
+# Example: In the data set [1, 2, 3, 4, 5],
+# the standard deviation is approximately 1.41,
+# which indicates that the values are relatively close to the mean (3).
+# In contrast, in the data set [1, 1, 1, 1, 5],
+# the standard deviation is approximately 1.60,
+# which indicates that the values are more spread out from the mean (1.8).
 def std_deviation(nums):
     n = len(nums)
     vals_mean = numpy.mean(nums)
