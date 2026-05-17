a0e4046e0963af1ad808c32329ee0794d89a989c
dmslob <dmytro.slobodenyuk@gmail.com>
2026-05-17 20:55:04 +0300

    bitarrays is updated

--- data/bitarrays.py
+++ data/bitarrays.py
@@ -13,12 +13,12 @@ x = bitarray(2 ** 20)  # bitarray of length 1048576 (initialized to 0)
 print(len(x))
 
 lst = [1, 0, False, True, True]
-a = bitarray(lst)      # initialize from iterable
+a = bitarray(lst)
 print(a)
 
 # Like lists, bitarray objects support slice assignment and deletion
 a = bitarray(50)
-a.setall(0)            # set all elements in a to 0
+a.setall(0)
 a[11:37:3] = 9 * bitarray('1')
 print(a)
 
